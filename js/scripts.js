7ac79bbe05d62daa6aa881b2e68262541528c639
Claire Felt <cfeltgungatan@gmail.com>
2024-11-25 16:40:08 -0700

    Website framework and photos

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+const navElement = document.querySelector('#theNav');
+const btnElement = document.querySelector('#theButton');
+
+btnElement.addEventListener('click', () => {
+    btnElement.classList.toggle('open');
+    navElement.classList.toggle('open');
+});
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    effect: 'slide',
+    speed: 1500,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    //start autoplay
+    autoplay: {
+        delay: 5000,
+    },
+  });
+
+  //add year here
+
+

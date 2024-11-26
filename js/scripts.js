b906ae484180c348b59d3aa4c37ae387578e96a0
Claire Felt <cfeltgungatan@gmail.com>
2024-11-25 20:59:26 -0700

    Full site? Lots of coloring and font

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,29 @@ const swiper = new Swiper('.swiper', {
     },
   });
 
-  //add year here
+// stuff to toggle titles based on screen size
+function adjustTitleBasedOnScreenSize() {
+  var urbanForestryTitle = document.getElementById("urbanForestryTitle");
+  var ufTitle = document.getElementById("ufTitle");
 
+  if (window.innerWidth < 768) {
+      // show UF on small screens
+      ufTitle.style.display = "block";
+      urbanForestryTitle.style.display = "none";
+  } else {
+      // show Urban Forestry on larger screens
+      urbanForestryTitle.style.display = "block";
+      ufTitle.style.display = "none";
+  }
+}
+
+// adjust title when the page is loaded
+window.addEventListener("load", adjustTitleBasedOnScreenSize);
+
+// adjust title whenever the window is resized
+window.addEventListener("resize", adjustTitleBasedOnScreenSize);
+
+//Put current year in footer
+const theYear = new Date();
+document.querySelector('#year').textContent = theYear.getFullYear()
 
